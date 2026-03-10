6a95c924f512715d7b8b76195e2b5b6b77c1a447
Adames_123 <e.adames11@unapec.edu.do>
2026-03-10 17:52:28 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_Documentos_por_Pagar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
